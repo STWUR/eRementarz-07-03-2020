c9739a6402c1c9b9f096535ab0b1058eaedcb0bd
Mateusz Staniak <mtst@mstaniak.pl>
2020-03-05 15:20:51 +0100

    Update configure.R

--- configure.R
+++ configure.R
@@ -1,5 +1,5 @@
-install.packages(packages = c("tidyverse", "mgcv", 
-                              "gam", "ggbeeswarm", "ggrepel"), 
+install.packages(pkgs = c("tidyverse", "mgcv", 
+                          "gam", "ggbeeswarm", "ggrepel"), 
                  repos = "https://cloud.r-project.org")
 gd <- read.csv("https://github.com/STWUR/eRementarz-07-03-2020/raw/master/data/ncn_grants.csv")
 write.csv(gd, file = "ncn_grants.csv", row.names = FALSE)
